54346aa62713938809571779b69583e6f0c3a528
MDuckkk <bmd040510@gmail.com>
2025-08-04 00:27:12 +0700

    feat: add SinhVien activity layout

---
+++ Android/app/src/main/java/com/bachld/android/SinhVienActivity.kt
@@ -0,0 +1,20 @@
+package com.bachld.android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SinhVienActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sinhvien)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.student)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
