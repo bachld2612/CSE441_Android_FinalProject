76c4394bccdd780ddaa03f90a52a353a820cf78b
bachld2612 <lyduybach900@gmail.com>
2025-08-08 16:26:15 +0700

    feat: create CRUD api for khoa feature

---
+++ Backend/src/main/java/com/bachld/project/backend/dto/response/khoa/KhoaResponse.java
@@ -0,0 +1,15 @@
+package com.bachld.project.backend.dto.response.khoa;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class KhoaResponse {
+
+    String tenKhoa;
+
+}
