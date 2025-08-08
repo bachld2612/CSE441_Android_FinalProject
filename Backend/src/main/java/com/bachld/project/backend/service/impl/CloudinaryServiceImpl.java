39e5c4d6d14b8a9c9964d0608f3d87f96c61334a
bachld2612 <lyduybach900@gmail.com>
2025-08-08 22:32:23 +0700

    feat: setup cloudinary config

---
+++ Backend/src/main/java/com/bachld/project/backend/service/impl/CloudinaryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.bachld.project.backend.service.impl;
+
+
+import com.cloudinary.Cloudinary;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class CloudinaryServiceImpl implements com.bachld.project.backend.service.CloudinaryService {
+
+    Cloudinary cloudinary;
+
+    @Override
+    public String upload(MultipartFile file) {
+        try {
+            Map result = cloudinary.uploader().upload(file.getBytes(), Map.of());
+            return result.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
