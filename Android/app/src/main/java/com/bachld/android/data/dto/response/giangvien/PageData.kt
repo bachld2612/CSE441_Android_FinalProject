d52ce0ed68a666a62f1a5fbdd633c8429b3ad085
bachld2612 <lyduybach900@gmail.com>
2025-08-23 11:11:11 +0700

    feat: add thong bao he thong on header

---
+++ Android/app/src/main/java/com/bachld/android/data/dto/response/giangvien/PageData.kt
@@ -0,0 +1,13 @@
+package com.bachld.android.data.dto.response.giangvien
+
+data class PageData<T>(
+    val content: List<T> = emptyList(),
+    val last: Boolean = true,
+    val totalPages: Int = 0,
+    val totalElements: Int = 0,
+    val number: Int = 0,
+    val size: Int = 0,
+    val first: Boolean = true,
+    val numberOfElements: Int = 0,
+    val empty: Boolean = true
+)
